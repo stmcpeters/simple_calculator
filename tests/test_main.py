11ea08c998c460d35652bb9dd64f6d855f788874
stmcpeters <stmcpeters@gmail.com>
2026-08-23 17:03:04 -0700

    adds tests to test add function correctly adds arguments given

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,3 +1,19 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from simple_calculator.main  import SimpleCalculator
+
+def test_add_two_numbers():
+    calculator = SimpleCalculator()
+
+    result = calculator.add(4,5)
+
+    assert result == 9
+
+def test_add_three_numbers():
+    calculator = SimpleCalculator()
+
+    result = calculator.add(4, 5, 6)
+
+    assert result == 15
+    
