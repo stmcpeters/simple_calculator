be1341231ade825004001d1a1ef2eeec7a09ab6d
stmcpeters <stmcpeters@gmail.com>
2026-08-23 17:17:03 -0700

    creates function that can multiply multiple numbers

--- simple_calculator/main.py
+++ simple_calculator/main.py
@@ -1,7 +1,15 @@
 # -*- coding: utf-8 -*-
+from functools import reduce
+
 class SimpleCalculator:
     def add(self, *args):
       return sum(args)
 
     def sub(self, a, b):
       return a - b
+
+    def mul(self, *args):
+      def mul2(a, b):
+        return a * b
+
+      return reduce(mul2, args)
